0ac29b21623917ea2729127e4709a024750f31c1
Blitzliner <manuel.jasch@gmx.de>
2020-04-17 12:17:40 +0200

    fixed bug in generating lines for eppaer.

--- common/epaper.py
+++ common/epaper.py
@@ -18,46 +18,43 @@ import logging
 #logging.basicConfig(level=logging.INFO, filename='epaper.log', filemode='w', format='%(asctime)s %(levelname)s (%(name)s): %(message)s')
 logger = logging.getLogger(__name__)
 
-def _read_image(path):
+def _read_image(path, max_width=800, max_height=600):
     logger.info("Slice image")
     lines = []
     if os.path.isfile(path):
         start_row_px = 0
-        img = np.array(Image.open(path).convert('L').resize((800, 600))).astype(np.uint8)
+        img = np.array(Image.open(path).convert('L').resize((max_width, max_height))).astype(np.uint8)
         bitmask = img>128
         img[bitmask] = 1
         img[~bitmask] = 0
         logger.info(F"Slice Image with size {img.shape}")
 
-        #if len(img[0,0]) == 3:
-        #    logger.error("wrong image format it has to be grayscale")
         # iterate over all columns
-        for col_idx in range(img.shape[1]):
+        for col_idx in range(max_width):
             #col = img[:,col_idx]
-            for row_idx in range(img.shape[0]):
+            for row_idx in range(max_height):
                 val = img[row_idx, col_idx]
                 if not start_row_px and not val: # start of line detected
                     start_row_px = row_idx
-                if start_row_px and val: # end of line detected
+                if (start_row_px and (val or (row_idx==(max_height-1)))): # end of line detected
                     x1 = col_idx
                     y1 = start_row_px
                     x2 = x1
                     y2 = row_idx
                     lines.append([x1,y1,x2,y2])
                     start_row_px = 0
-    if len(lines) > 20000:
-        logger.warning(F"Image is maybe to detailed. Too many lines are generated ({len(lines)})")
-        lines = lines[:20000]
+    if len(lines) > 40000:
+        logger.warning(F"Image is too detailed. Detected ({len(lines)}) lines. Limit to 40000.")
+        lines = lines[:40000]
 
     return lines
 
 
 def send(path):
-    logger.info("Send Image to E-Paper..")
     start = time.time()
 
     lines = _read_image(path)
-    logger.info(F"Splitte Image to {len(lines)} lines")
+    logger.info(F"Send Image.. Splitt Image to {len(lines)} lines")
 
     with EPaper() as paper:
         paper.send(Handshake())
