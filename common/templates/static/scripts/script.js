118d96d5898586d207d80825a4ed8595002ad08b
Manuel Jasch <manuel.jasch@zf.com>
2024-12-31 14:33:11 +0100

    Add snackbar for preview

--- common/templates/static/scripts/script.js
+++ common/templates/static/scripts/script.js
@@ -1,39 +1,44 @@
+'use strict'
+
 function loadAddressInIframe(eventElement) {
     // get form to which the preview belongs to
     var form = eventElement.parentNode;
-    // get the two fieldsets
-    var fields = form.querySelectorAll("fieldset");
+    // get the app name
+    var app_name = form.querySelector('input[name="app_name"]').value;
     // the first fieldset are the general settings, and the second input is the address
-    var address = fields[0].querySelectorAll('input[name="app_base_address"')[0].value;
+    var address = form.querySelector('input[name="app_base_address"').value;
     console.log("address: " + address);
-    // the second fieldset is the parameter list
-    var params = fields[1].querySelectorAll("input");
-    var params_arr = []
-    // loop over all parameters and add to list
-    for (i=0; i<params.length; i++){
-        var p = params[i];
-        params_arr.push(encodeURIComponent(p.name) + "=" + encodeURIComponent(p.value));
-    }
-    var params_str = params_arr.join("&");
-    // add parameters to the address
-    if (params_str.length > 0) {
-        address += "?" + params_str;
-        console.log("address with parameters: " + address);
+    // the second fieldset is the parameter list check if it is available
+    var fields = form.querySelectorAll("fieldset");
+    if (fields.length > 1) {
+        var params = fields[1].querySelectorAll("input");
+        var params_arr = []
+        // loop over all parameters and add to list
+        for (var i = 0; i < params.length; i++){
+            var p = params[i];
+            params_arr.push(encodeURIComponent(p.name) + "=" + encodeURIComponent(p.value));
+        }
+        var params_str = params_arr.join("&");
+        // add parameters to the address
+        if (params_str.length > 0) {
+            address += "?" + params_str;
+            console.log("address with parameters: " + address);
+        }
     }
     // set source property of iframe
-    var preview = document.getElementById("iframe_preview");
+    const preview = document.getElementById("iframe_preview");
     preview.setAttribute("src", address);
+    showSnackbar('Preview website for ' + app_name);
     _update_address(address);
 }
 
 function _update_address(address) {
-    preview = document.getElementById("preview_address");
+    const preview = document.getElementById("preview_address");
     preview.value = address;
 }
 
 function loadImageInIframe(eventElement) {
     var file = document.getElementById("file_upload").files[0];
-    console.log(file);
     if (file) {
         // Make a file reader to interpret the file
         var reader = new FileReader();
@@ -44,10 +49,20 @@ function loadImageInIframe(eventElement) {
         };
         // Tell the reader to start reading asynchrounously
         reader.readAsDataURL(file);
+        showSnackbar('Show image preview');
     } else {
-        console.log("no file selected");
+        showSnackbar('No Image selected');
     }
     _update_address('');
+}
+
+function showSnackbar(message) {
+    const snackbar = document.getElementById('snackbar');
+    snackbar.innerHTML = message;
+    snackbar.className = 'show';
+    setTimeout(function() {
+        snackbar.className = snackbar.className.replace("show", "");
+    }, 3000);
 }
 
 // wait for document is ready
@@ -66,4 +81,6 @@ document.addEventListener('DOMContentLoaded', function(event) {
             }
         }, true);
     }
+    /* snackbar */
+
 })
