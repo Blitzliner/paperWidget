46874f7c9983867f27f5a98e4fa836c78e21ab6e
Manuel Jasch <manuel.jasch@zf.com>
2025-01-02 13:32:05 +0100

    Set to gray colors

--- common/epaper.py
+++ common/epaper.py
@@ -8,25 +8,26 @@ logger = utils.getLogger()
 
 @utils.timing
 def send(path):
-    img = read_image(path=path, bit_depth=1)
+    img = read_image(path=path, bit_depth=2)
     rects = get_shapes(img=img, slicer=SliceOptions.SLICE_RECTS_OPT_INLINE, value=0)
     baudrate = 115200  # bits/s
     command_length = 17 * 8  # bits
-    cost = (command_length * len(rects)) / baudrate
-    logger.info(f'Image will take approx. {cost:.3f} s')
 
     with EPaper() as paper:
         paper.send(Handshake())
         time.sleep(0.1)
-        # use of DARK_GRAY instead of BLACK for a more clear image
-        paper.send(SetPallet(SetPallet.DARK_GRAY, SetPallet.WHITE))
         paper.send(SetCurrentDisplayRotation(SetCurrentDisplayRotation.FLIP))
-        paper.read_responses(timeout=10)
-
-        for idx, rect in enumerate(rects):
-            paper.send(FillRectangle(x1=rect[0], y1=rect[1], x2=rect[2], y2=rect[3]))
-            if idx % 1000 == 0:
-                logger.info(f'Send {idx}/{len(rects)}')
+        colors = {0: SetPallet.BLACK, 1: SetPallet.DARK_GRAY, 2: SetPallet.LIGHT_GRAY}  # , 3: SetPallet.WHITE}
+        for value, color in colors.items():
+            rects = get_shapes(img=img, slicer=SliceOptions.SLICE_RECTS_OPT_INLINE, value=value)
+            logger.info(f'Image will take approx. {(command_length * len(rects)) / baudrate:.3f} s')
+            # use of DARK_GRAY instead of BLACK for a more clear image
+            paper.send(SetPallet(color, SetPallet.WHITE))
+            # paper.read_responses(timeout=10)
+            for idx, rect in enumerate(rects):
+                paper.send(FillRectangle(x1=rect[0], y1=rect[1], x2=rect[2], y2=rect[3]))
+                if idx % 1000 == 0:
+                    logger.info(f'Send {idx}/{len(rects)}')
         paper.send(RefreshAndUpdate())  # does this take approx 8s?
         paper.read_responses()
 
